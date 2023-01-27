316c34b01bf211c2f5be6960f82d73be5295abc5
AnnaAmurskaya <amursckaya2010@yandex.ru>
2023-01-27 19:36:24 +0300

    Added task solution

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Введи цифру, обозначающую день недели");
+
+int a = int.Parse(ReadLine()!);
+
+if (a >= 1 && a <= 7)
+{
+    if (a == 6 || a == 7) Write("да");
+    else Write("нет");
+}
+else WriteLine("нет такого дня недели");
