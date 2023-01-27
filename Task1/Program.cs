43621fa0d4bf1d6b7b775939d63d0212638e9e5d
AnnaAmurskaya <amursckaya2010@yandex.ru>
2023-01-27 19:15:41 +0300

    Added Task solution

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+Write("Введите число:");
+int n = int.Parse(ReadLine()!);
+
+if(n>=100 && n <=999)
+{
+    n /= 10;
+    n %= 10;
+    WriteLine(n);
+}
+else WriteLine("Введено не трехзначное число");
+
